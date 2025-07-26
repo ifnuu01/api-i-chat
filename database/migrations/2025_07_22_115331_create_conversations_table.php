fb3839c80778ab9cea287cedf5be5763f5cb7137
ifnuu01 <ifnuu01@gmail.com>
2025-07-26 19:33:45 +0800

    change field

--- database/migrations/2025_07_22_115331_create_conversations_table.php
+++ database/migrations/2025_07_22_115331_create_conversations_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user1_id')->constrained('users', 'id')->onDelete('cascade');
             $table->foreignId('user2_id')->constrained('users', 'id')->onDelete('cascade');
-            $table->dateTime('user1_last_read_at');
-            $table->dateTime('user2_last_read_at');
-            $table->dateTime('last_message_at');
-            $table->softDeletes();
+            $table->dateTime('user1_last_read_at')->nullable();
+            $table->dateTime('user2_last_read_at')->nullable();
+            $table->dateTime('last_message_at')->nullable();
             $table->timestamps();
+
             $table->unique(['user1_id', 'user2_id']);
             $table->index(['user1_id', 'user2_id']);
             $table->index('last_message_at');
